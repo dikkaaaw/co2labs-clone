82d457844f344e2ff603f65b15d9d00f758c86d5
Dika Wicaksono <dhkwck00@gmail.com>
2024-09-30 00:11:46 +0700

    Refactor routing in App component: Replace AboutUs, Portfolio, and Contact with FaqSection and TermsAndPolicy components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,3 @@
 export { default as Homepage } from "./Homepage.jsx";
-export { default as AboutUs } from "./AboutUs.jsx";
-export { default as Portfolio } from "./Portfolio.jsx";
-export { default as Contact } from "./Contact.jsx";
+export { default as FaqSection } from "./faqSection.jsx";
+export { default as TermsAndPolicy } from "./TermsAndPolicy.jsx";
